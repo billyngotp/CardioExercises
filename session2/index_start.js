3dbc0e9ffb7d48023007a5b2d85c29f284d9f356
Billy Ngo <billyngo@talentpath.com>
2019-10-15 06:30:10 -0500

    add unfinished session 2

--- session2/index_start.js
+++ session2/index_start.js
@@ -3,9 +3,31 @@
 // ex. longestWord('Hi there, my name is Brad') === 'there,'
 
 function longestWord(sen) {
-  // SOLUTION 1 - Return a single longest word
+  // SOLUTION 1 - Return a single longest word  
   // SOLUTION 2 - Return an array and include multiple words if they have the same length
   // SOLUTION 3 - Only return an array if multiple words, otherwise return a string
+
+  //new array for multiple words 
+  formattedSent = sen.split(' ');
+  let longWord = ""
+  let arrayWords = []
+  arrayWords.push(formattedSent[0]);
+  console.log(arrayWords);
+  for (i in formattedSent) {
+
+    if(formattedSent[i].length >arrayWords[0].length)
+      console.log(sen[i] + " is element at i");
+      arrayWords.push(formattedSent[i]);
+    if formattedSent[i].length === arrayWords[0].length)
+    {
+      arrayWords.push(formattedSent[i]);
+    }
+    else{
+      arrayWords.pull();
+    }
+  }
+
+  console.log(arrayWords);
 }
 
 // CHALLENGE 2: ARRAY CHUNKING
@@ -26,12 +48,22 @@ function flattenArray(arrays) {}
 // ex. 'elbow' === 'below'
 // ex. 'Dormitory' === 'dirty room##'
 
-function isAnagram(str1, str2) {}
+function isAnagram(str1, str2) {
+  return formatStr(str1) === formatStr(str2);
+}
 
 // CHALLENGE 5: LETTER CHANGES
 // Change every letter of the string to the one that follows it and capitalize the vowels
 // Z should turn to A
 // ex. 'hello there' === 'Ifmmp UIfsf'
+function formatStr(str) {
+  return str
+    .replace(/[^\w]/g, '')
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('');
+}
 
 function letterChanges(str) {}
 
